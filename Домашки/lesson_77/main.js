6d9ef0ad1fa9cceab6348167e3840ff2b2d88214
UBTdev <156011589+UBTdev@users.noreply.github.com>
2024-09-14 01:11:08 +0300

    Add files via upload

---
+++ Домашки/lesson_77/main.js
@@ -0,0 +1,11 @@
+const userFirstName = 'Michail'
+const userLastName = ' Khruschov'
+const userGender = ' Male'
+let userCountry = ' Russia'
+let userAge = 19
+
+console.log('First Name:', userFirstName)
+console.log('Last Name:', userLastName)
+console.log('Gender:', userGender)
+console.log('Country:', userCountry)
+console.log('Age', userAge, 'Y.o')
